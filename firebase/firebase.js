ec373369fbf61e97fe01c3c2b1b6ed31df48f9f4
Jorge Navarrete <103383406+kokynavarrete@users.noreply.github.com>
2022-06-30 17:37:58 -0400

    Primer commit

---
+++ firebase/firebase.js
@@ -0,0 +1,29 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.2/firebase-app.js";
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/9.8.2/firebase-firestore.js"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBcfiqqENKNjijCd5L9t6D0ImcE_ULbnGo",
+    authDomain: "proyecto-jardin-firebase.firebaseapp.com",
+    projectId: "proyecto-jardin-firebase",
+    storageBucket: "proyecto-jardin-firebase.appspot.com",
+    messagingSenderId: "1043248227049",
+    appId: "1:1043248227049:web:feda24c001f6ded7868678"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+//Conexion a db
+const db = getFirestore()
+
+
+export const guardarParvularia = (nombre, rut, edad, direccion) => {
+    addDoc(collection(db, 'parvularias'), {
+        nombre: nombre, rut: rut, edad: edad, direccion: direccion
+    })
+}
---
+++ index.js
@@ -0,0 +1,26 @@
+
+import { guardarParvularia } from './firebase/firebase.js';
+
+
+//Evento que arranca la app
+window.addEventListener('DOMContentLoaded', () => {
+    console.log("trabajando")
+});
+
+const formularioParbularia = document.getElementById('formulario-agregar-parvularia');
+
+
+formularioParbularia.addEventListener('submit', (e) => {
+    e.preventDefault()
+    console.log('Submited')
+    const nombre = formularioParbularia['nombreP'];
+    const rut = formularioParbularia['rutP'];
+    const edad = formularioParbularia['edadP'];
+    const direccion = formularioParbularia['direccionP'];
+
+    guardarParvularia(nombre.value, rut.value, edad.value, direccion.value)
+
+    formularioParbularia.reset()
+
+
+});
